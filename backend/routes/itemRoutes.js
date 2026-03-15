31b6339ea085d7f5be622fb6ff628c3877fc15ad
R.M <91456939+RMANHAS@users.noreply.github.com>
2026-03-15 16:34:01 +0530

    Update itemRoutes.js

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -1,15 +1,9 @@
-// const router = require("express").Router();
-// const { createItem, getItems } = require("../controllers/itemController");
 
-// router.post("/", createItem);
-// router.get("/", getItems);
-
-// module.exports = router;
 const express = require("express");
 const router = express.Router();
 const { createItem, getItems } = require("../controllers/itemController");
 
-router.post("/", createItem); // Create item
-router.get("/", getItems); // Get all items
+router.post("/", createItem); 
+router.get("/", getItems);
 
 module.exports = router;
